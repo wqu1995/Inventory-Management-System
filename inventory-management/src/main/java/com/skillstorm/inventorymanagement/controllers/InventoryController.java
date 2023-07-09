3c0c65c3b8fd648433a962bb1acb128c5ce53acb
Wenjun Qu <wqu1995@gmail.com>
2023-07-08 21:00:52 -0400

    Added basic CRUD functionality for Inventory

---
+++ inventory-management/src/main/java/com/skillstorm/inventorymanagement/controllers/InventoryController.java
@@ -0,0 +1,71 @@
+package com.skillstorm.inventorymanagement.controllers;
+
+import com.skillstorm.inventorymanagement.models.Inventory;
+import com.skillstorm.inventorymanagement.models.InventoryId;
+import com.skillstorm.inventorymanagement.services.InventoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/inventories")
+public class InventoryController {
+    @Autowired
+    InventoryService inventoryService;
+
+    /**
+     * Method to handle GET("/inventories") request
+     *
+     * @return the response entity
+     */
+    @GetMapping
+    public ResponseEntity<List<Inventory>> findAllInventories(){
+        List<Inventory> inventories = inventoryService.findAllInventories();
+
+        return new ResponseEntity<>(inventories, HttpStatus.OK);
+    }
+
+    /**
+     * Method to handle POST("/inventories/addInventory") request
+     *
+     * @param inventoryToBeAdded the inventory to be added
+     * @return the response entity
+     */
+    @PostMapping("/addInventory")
+    public ResponseEntity<Inventory> addInventory(@RequestBody Inventory inventoryToBeAdded){
+        Inventory newInventory = inventoryService.addInventory(inventoryToBeAdded);
+
+        return new ResponseEntity<>(newInventory, HttpStatus.CREATED);
+    }
+
+    /**
+     * Method to handle PUT("/inventories/updateInventory") request
+     *
+     * @param inventoryToBeUpdated the inventory to be updated
+     * @return the response entity
+     */
+    @PutMapping("/updateInventory")
+    public ResponseEntity<Inventory> updateInventory(@RequestBody Inventory inventoryToBeUpdated){
+        Inventory updatedInventory = inventoryService.addInventory(inventoryToBeUpdated);
+
+        return new ResponseEntity<>(updatedInventory, HttpStatus.ACCEPTED);
+
+    }
+
+    /**
+     * Method to handle DELETE("/inventories/deleteInventory") request
+     *
+     * @param inventoryToBeDeleted the inventory to be deleted
+     * @return the response entity
+     */
+    @DeleteMapping("/deleteInventory")
+    public ResponseEntity<Integer> deleteInventory(@RequestBody Inventory inventoryToBeDeleted){
+        int rowAffected = inventoryService.deleteInventory(inventoryToBeDeleted);
+
+        return new ResponseEntity<>(rowAffected, HttpStatus.OK);
+    }
+}
+
---
+++ inventory-management/src/main/java/com/skillstorm/inventorymanagement/repositories/InventoryRepository.java
@@ -0,0 +1,19 @@
+package com.skillstorm.inventorymanagement.repositories;
+
+import com.skillstorm.inventorymanagement.models.Inventory;
+import com.skillstorm.inventorymanagement.models.InventoryId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public interface InventoryRepository extends JpaRepository<Inventory, InventoryId> {
+
+    @Query(value = "DELETE FROM Inventory i WHERE i.id = ?1")
+    @Modifying
+    @Transactional
+    int costumeDeleteById(InventoryId id);
+
+}
