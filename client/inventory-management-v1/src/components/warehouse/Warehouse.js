298e57e350f5377ee09074de9c210b362a54e182
Wenjun Qu <wqu1995@gmail.com>
2023-07-11 15:04:49 -0400

    Added functionality to update warehouse from the website

--- client/inventory-management-v1/src/components/warehouse/Warehouse.js
+++ client/inventory-management-v1/src/components/warehouse/Warehouse.js
@@ -2,17 +2,15 @@ import React, { useState, useEffect } from 'react';
 import api from '../../api/axiosConfig';
 import { Card } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
-import Modal from 'react-bootstrap/Modal';
-import Form from 'react-bootstrap/Form';
+import AddWarehouseModal from './AddWarehouseModal';
+import EditWarehouseModal from './EditWarehouseModal';
+
 
 function Warehouse() {
 
-    const [warehouses, setWarehouses] = useState();
-    const [show, setShow] = useState(false);
-    const [addWarehouseData, updateAddedWarehouse] = useState();
-
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const [warehouses, setWarehouses] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+    const [editingWarehouse, setEditingWarehouse] = useState(null);
 
     const getWarehouses = () =>{
         api.get("/warehouses").then((response) =>{
@@ -22,29 +20,50 @@ function Warehouse() {
         })
     }
 
-    const addWarehouse =() =>{
-        api.post("/warehouses/addWarehouse", addWarehouseData).then((response) =>{
+    const handleAddWarehouse =(newWarehouse) =>{
+        api.post("/warehouses/addWarehouse", newWarehouse).then((response) =>{
             if(response.status===201 && response.data){
-                setWarehouses(oldArray => [...oldArray, response.data]);
+                setWarehouses([...warehouses, response.data]);
             }
         }).catch((error) =>{
             console.log(error);
         })
     }
 
-    const handleChange = (e) =>{
-        updateAddedWarehouse({
-            ...addWarehouseData,
-            [e.target.name] : e.target.value.trim()
+    const handleEditWarehouse = (editedWarehouse) =>{
+        api.put("/warehouses/updateWarehouse", editedWarehouse).then((response)=>{
+            if(response.status===202 && response.data){
+                setWarehouses(prevWarehouses =>{
+                    const updatedWarehouses = prevWarehouses.map(warehouse =>{
+                        if(warehouse.id === response.data.id){
+                            return response.data;
+                        }
+                        return warehouse;
+                    });
+                    return updatedWarehouses;
+                });
+            }
+        }).catch((error)=>{
+            console.log(error)
         })
     }
 
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-        addWarehouse();
-        //console.log(addWarehouseData);
-        handleClose();
+    const handleEditModalOpen = (warehouse) =>{
+        setEditingWarehouse(warehouse);
     }
+
+    const handleEditModalClose = () =>{
+        setEditingWarehouse(null);
+    }
+
+    const handleCloseModal = () =>{
+        setShowModal(false);
+    }
+
+    const handleShowModal = () =>{
+        setShowModal(true);
+    }
+
 
     useEffect(()=>{
         getWarehouses();
@@ -55,55 +74,41 @@ function Warehouse() {
         <div>
             <div className='d-flex justify-content-between align-items-center mb-3'>
                 <h1 className="text-dark">Warehouses</h1>
-                <Button variant = "primary" className='custom-button' onClick={handleShow}>Add Warehouse</Button>
+                <Button variant = "primary" className='custom-button' onClick={handleShowModal}>Add Warehouse</Button>
             </div>
-
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Add Warehouse</Modal.Title>
-                </Modal.Header>
-
-                <Modal.Body>
-                    <Form>
-                        <Form.Group className='mb-3' controlId='addWarehouseName'>
-                            <Form.Label>Name</Form.Label>
-                            <Form.Control name='name' onChange={handleChange} type='text' placeholder='Enter warehouse name'/>
-                        </Form.Group>
-                        <Form.Group className='mb-3' controlId='addWarehouseLocation'>
-                            <Form.Label>Location</Form.Label>
-                            <Form.Control name='location' onChange={handleChange} type='text' placeholder='Enter warehouse location'/>
-                        </Form.Group>
-                        <Form.Group className='mb-3' controlId='addWarehouseSize'>
-                            <Form.Label>Warehouse Size</Form.Label>
-                            <Form.Control name='size' onChange={handleChange} type='number' placeholder='Enter warehouse size'/>
-                        </Form.Group>
-                        <Form.Group className='mb-3' controlId='addWarehouseCapacity'>
-                            <Form.Label>Warehouse Capacity</Form.Label>
-                            <Form.Control name='capacity' onChange={handleChange} type='number' placeholder='Enter warehouse capacity'/>
-                        </Form.Group>
-                    </Form>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant='secondary' onClick={handleClose}>Close</Button>
-                    <Button variant='primary' onClick={handleSubmit}>Add!</Button>
-                </Modal.Footer>
-
-            </Modal>
 
             {warehouses?.map((warehouse)=>(
                 <Card key={warehouse.id} className='mb-3 custom-card'>
-                    <div className='d-flex justify-content-between align-items-center mb-3'>
-                        <Card.Body>
-                            <Card.Title>{warehouse.name}</Card.Title>
-                            <Card.Text>{warehouse.location}</Card.Text>
-                            <Card.Text>Size: {warehouse.size} Capacity: {warehouse.capacity}</Card.Text>
-                        </Card.Body>
-                        <Button variant="primary" className="custom-button mb-3" onClick={handleShow}>Edit</Button>
-                        <Button variant="secondary" className="custom-button mb-3" onClick={handleShow}>Delete</Button>
-                    </div>
+                    <Card.Body>
+                        <div className="d-flex justify-content-between align-items-center">
+                            <div>
+                                <Card.Title>{warehouse.name}</Card.Title>
+                                <Card.Text>{warehouse.location}</Card.Text>
+                                <Card.Text>Size: {warehouse.size} Capacity: {warehouse.capacity}</Card.Text>
+                            </div>
+                            <div>
+                            <Button variant="outline-primary" size="sm" onClick={() => handleEditModalOpen(warehouse)}> Edit</Button>
+                            <span className="mr-2"></span> {/* Add spacing between buttons */}
+                            <Button variant="outline-danger" size="sm"> Delete</Button>
+                            </div>
+                        </div>
+                    </Card.Body>
 
                 </Card>
             ))}
+
+            <AddWarehouseModal
+                show = {showModal}
+                handleClose={handleCloseModal}
+                handleAddWarehouse={handleAddWarehouse}
+            />
+
+            <EditWarehouseModal
+                show = {!!editingWarehouse}
+                handleClose={handleEditModalClose}
+                warehouse={editingWarehouse}
+                handleEditWarehouse={handleEditWarehouse}
+            />
         </div>
     )
 }
